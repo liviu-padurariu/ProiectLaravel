58c0ff410826b58716f466b5884dee833bb6db38
Liviu <padurariuliviu@gmail.com>
2024-01-04 18:27:29 +0200

    Add: article model

---
+++ app/Models/Article.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Article extends Model
+{
+    use HasFactory;
+    // $table->id('article_id');
+    // $table->integer('user_id');
+    // $table->string('title', 255);
+    // $table->text('content');
+    // $table->integer('category_id');
+    // $table->dateTime('submission_date');
+    // $table->boolean('is_approved');
+    protected $fillable = [
+        'user_id', 'title', 'content', 'category_id', 'submission_date', 'is_approved'
+    ];
+}
