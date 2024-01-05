ab835971fc0461b89e2347b83fdf790faaed762c
Liviu <padurariuliviu@gmail.com>
2024-01-05 19:31:48 +0200

    Mod: nume nav bar

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -8,8 +8,7 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <!-- The above 2 meta tags *must* come first in the head; any other
-head content must come *after* these tags -->
+  <!-- The above 2 meta tags *must* come first in the head; any other head content must come *after* these tags -->
   <meta name="description" content="">
   <meta name="author" content="">
   <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
@@ -20,7 +19,7 @@ head content must come *after* these tags -->
     <nav class="navbar navbar-default">
       <div class="container-fluid">
         <div class="navbar-header">
-          <a class="navbar-brand" href="{{ URL::to('home') }}">Exemplu aplicatie-Laravel CRUD </a>
+          <a class="navbar-brand" href="{{ URL::to('home') }}">Proiectare site-uri WEB Proiect Laravel!</a>
         </div>
         <ul class="nav navbar-nav">
         </ul>
