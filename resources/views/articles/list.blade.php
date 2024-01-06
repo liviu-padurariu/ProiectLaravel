73744f37ddd560ca7632b1c37bf8cd69334696d5
Liviu <padurariuliviu@gmail.com>
2024-01-06 14:56:50 +0200

    Fix: articles list

--- resources/views/articles/list.blade.php
+++ resources/views/articles/list.blade.php
@@ -12,15 +12,18 @@
   <div class="panel-body">
     <div class="form-group">
       <div class="pull-right">
-        <a href="/articles/create" class="btn btn-default">Adaugare
-          Sarcina Noua</a>
+        <a href="/articles/create" style="margin: 10px" class="btn btn-info">Adaugare Article Noua</a>
       </div>
     </div>
     <table class="table table-bordered table-stripped">
       <tr>
         <th width="20">No</th>
         <th>Titlu</th>
-        <th>Descriere</th>
+        <th>Content</th>
+        <th>User ID</th>
+        <th>Category ID</th>
+        <th>Submission Date</th>
+        <th>Approved</th>
         <th width="300">Actiune</th>
       </tr>
       @if (count($articles) > 0)
@@ -29,11 +32,15 @@
         <td>{{ ++$i }}</td>
         <td>{{ $article->title }}</td>
         <td>{{ $article->content }}</td>
+        <td>{{ $article->user_id }}</td>
+        <td>{{ $article->category_id }}</td>
+        <td>{{ $article->submission_date }}</td>
+        <td>{{ $article->is_approved }}</td>
         <td>
           <a class="btn btn-success" href="{{route('articles.show',$article->article_id) }}">Vizualizare</a>
           <a class="btn btn-primary" href="{{route('articles.edit',$article->article_id) }}">Modificare</a>
           {{ Form::open(['method' => 'DELETE','route' => ['articles.destroy', $article->article_id],'style'=>'display:inline']) }}
-          {{ Form::submit('Delete', ['class' => 'btn btndanger']) }}
+          {{ Form::submit('Delete', ['class' => 'btn btn-danger']) }}
           {{ Form::close() }}
         </td>
       </tr>
