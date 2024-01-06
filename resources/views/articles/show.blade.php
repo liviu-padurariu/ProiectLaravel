c7524c39ce5cd4d0b13217f0537f30a0d282b7b1
Liviu <padurariuliviu@gmail.com>
2024-01-06 15:12:43 +0200

    Add: missing fields

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -9,10 +9,22 @@
       <a class="btn btn-default" href="{{ route('articles.index') }}">Inapoi</a>
     </div>
     <div class="form-group">
-      <strong>Nume: </strong> {{ $article->title }}
+      <strong>Titlu: </strong> {{ $article->title }}
     </div>
     <div class="form-group">
       <strong>Descriere: </strong> {{ $article->content }}
+    </div>
+    <div class="form-group">
+      <strong>User ID: </strong> {{ $article->user_id }}
+    </div>
+    <div class="form-group">
+      <strong>Category ID: </strong> {{ $article->category_id }}
+    </div>
+    <div class="form-group">
+      <strong>Submission Date: </strong> {{ $article->submission_date }}
+    </div>
+    <div class="form-group">
+      <strong>Approved: </strong> {{ $article->is_approved }}
     </div>
   </div>
 </div>
