240343dcf64e35c678e9ee3d508af9e4030f1710
Liviu <padurariuliviu@gmail.com>
2024-01-06 12:59:49 +0200

    Add: id to migration

--- database/migrations/2024_01_04_165424_create_user_categories_table.php
+++ database/migrations/2024_01_04_165424_create_user_categories_table.php
@@ -12,6 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('user_categories', function (Blueprint $table) {
+            $table->id();
             $table->integer('user_id');
             $table->integer('category_id');
             $table->timestamps();
